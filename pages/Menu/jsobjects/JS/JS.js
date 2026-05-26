916ff30eadb5df506efde077bc9d638b197b8b67
Daniel Aldana <daniel.aldana@pclguatemala.com>
2026-05-26 13:02:47 +0000

    Debug.

--- pages/Menu/jsobjects/JS/JS.js
+++ pages/Menu/jsobjects/JS/JS.js
@@ -1,5 +1,11 @@
 export default {
 	onLoad() {
+		
+		showAlert(
+						"sesion_id: " + appsmith.store.sesion_id +
+						" | autenticado: " + appsmith.store.usuario_autenticado
+				)
+
 		infoPCLAuth.protegerPagina("MENU");
 	}
 }
